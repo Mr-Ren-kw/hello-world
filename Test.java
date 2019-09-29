a026999e31df28ceae78683cefd7bdf33e6aa7e0
Mr-Ren-kw <335614025@qq.com>
2019-09-29 22:25:40 +0800

    user:renkw fifth change

---
+++ Test.java
@@ -0,0 +1,5 @@
+public class Test(){
+	public static void main(System[] args){
+		System.out.println("水水水水买买买");
+	}
+}
--- hello.java
+++
@@ -1,9 +0,0 @@
-class Jsh{
-	public static void main(String[] args){
-		System.out.println("我是蒋四海");
-		System.out.println("who are you");
-		
-	}
-	
-}
-	
